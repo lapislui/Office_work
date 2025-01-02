d05d7fac3718d45e6e9ac5fd0cd119668add142d
lapislui <patelkapila9978@gmail.com>
2025-01-02 10:29:13 +0530

    creation commit

--- docs/script.js
+++ docs/script.js
@@ -6,3 +6,18 @@ if (x==5) {
     console.log("output is true");
 }
 
+function example() {
+    if (true) {
+        let bv = "keval";
+        console.log(bv);
+    }
+
+}
+example();
+
+let fn = "keval";
+let ln = 'patel';
+let num1 =100;
+let num2 = 96.5;
+
+document.write(typeof(num1));
