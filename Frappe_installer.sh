8747145713cca3e64b315fa574369b3503a6921c
lapislui <lapislui1234@gmail.com>
2025-01-05 14:31:32 +0530

    Sun 05 Jan 2025 02:31:32 PM IST

--- Frappe_installer.sh
+++ Frappe_installer.sh
@@ -1,1 +1,14 @@
-sudo apt-get update -y && sudo apt-get upgrade -y
+# sudo apt-get update -y && sudo apt-get upgrade -y
+
+# sudo adduser frappe && usermod -aG sudo frappe && su hp cd /home/frappe
+
+# sudo apt-get install git
+
+# sudo apt-get install python3-dev python3-dev python3-setuptools python3-pip python3-distutils
+
+#  python3 --version
+
+#  sudo apt-get install python3.10-venv
+
+# sudo apt-get install software-properties-common
+
