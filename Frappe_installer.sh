bf4dd41597ebe9946a51e17360d1434d0f25384d
lapislui <lapislui1234@gmail.com>
2025-01-05 14:57:38 +0530

    Sun 05 Jan 2025 02:57:38 PM IST

--- Frappe_installer.sh
+++ Frappe_installer.sh
@@ -6,9 +6,41 @@
 
 # sudo apt-get install python3-dev python3-dev python3-setuptools python3-pip python3-distutils
 
+# echo "python-versio:"
 #  python3 --version
 
 #  sudo apt-get install python3.10-venv
 
 # sudo apt-get install software-properties-common
 
+# sudo apt install mariadb-server mariadb-client
+
+# sudo apt-get install redis-server
+
+# sudo apt-get install xvfb libfontconfig wkhtmltopdf 
+# sudo apt-get install libmysqlclient-dev
+
+# sudo mysql_secure_installation
+
+# sudo nano /etc/mysql/my.cnf
+
+# sudo service mysql restart
+
+# sudo apt install curl
+
+# curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+# curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# nano ~/.profile 
+# add following at end:
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# source ~/.profile 
+# nvm install 18
+
+# sudo apt-get install npm
+
+# sudo npm install -g yarn
+
+pip3 install frappe-bench
