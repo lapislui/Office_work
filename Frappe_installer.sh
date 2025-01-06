3bbf8f1f2e901224879e751478f0af1b390273f1
lapislui <lapislui1234@gmail.com>
2025-01-06 11:18:01 +0530

    Mon 06 Jan 2025 11:18:00 AM IST

--- Frappe_installer.sh
+++ Frappe_installer.sh
@@ -32,9 +32,9 @@
 # curl https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 # nano ~/.profile 
 # add following at end:
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # source ~/.profile 
 # nvm install 18
