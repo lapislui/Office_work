5969edc7a554d2b0da5bac6c688a435244104c6a
lapislui <lapislui1234@gmail.com>
2025-01-10 11:41:53 +0530

    feat: add Student class with initialization and display methods

---
+++ Python/python_revision.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+# Example usage
+student = Student("keval patel", 20)
+student.display()
