714532568a40a22c66312244e31f478049752973
lapislui <lapislui1234@gmail.com>
2025-01-10 12:24:53 +0530

    feat: add NewClass with initialization and method to display value; create tempCodeRunnerFile for testing

--- Python/python_revision.py
+++ Python/python_revision.py
@@ -17,4 +17,15 @@ a, b = b, a  # tuple packing and unpacking
 print("After swap: a =", a, "b =", b)
 
 
+class NewClass:
+    def __init__(self, x):
+        self.x = x
 
+    def func(self):
+        print("x =", self.x)
+
+obj = NewClass(10)
+obj.func()  # x = 10 
+
+
+print("Hello World")  # Hello World
---
+++ Python/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+class newclass:
+    def __init__(self):
+        self.x = 10
+    def func(self):
+        print("x= ", self.x)
+
+newclass()  # x= 10
+    
