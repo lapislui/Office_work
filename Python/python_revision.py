e3c84c81a8604528ca15b90d12dbcd0cef0de2dd
lapislui <lapislui1234@gmail.com>
2025-01-10 13:28:05 +0530

    feat: add Obj class with initialization and method to display value; include inner variable in Welcome class

--- Python/python_revision.py
+++ Python/python_revision.py
@@ -32,9 +32,25 @@ print("obj.x= ",obj.x)
 class Welcome:
     def __init__(self, message):
         self.message = message
-
+    innervar=15
     def welcome_func(self):
         print(self.message)
 
 welcome_obj = Welcome("Welcome to the Python programming world!")
 welcome_obj.welcome_func()  # Welcome to the Python programming world!
+print(welcome_obj.innervar)
+
+class Obj:
+    "this is Obj class"
+
+    def __init__(self, value):
+        self.value = value
+
+    def show_value(self):
+        print("Value:", self.value)
+
+obj_instance = Obj(42)
+obj_instance.show_value()  # Value: 42
+
+# Accessing the __doc__ attribute
+print(Obj.__doc__)
