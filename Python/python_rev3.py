91b965c812b74bfb070446e2e2d01d150554a722
lapislui <lapislui1234@gmail.com>
2025-01-11 11:42:52 +0530

    feat: add for loop and iterator implementation examples in Python

--- Python/python_rev3.py
+++ Python/python_rev3.py
@@ -27,3 +27,30 @@ while True:
     except StopIteration:
         break
 
+for x in mylist:
+    print(x)
+
+
+class myclass:
+    def __init__(self, x):
+        self.x = x
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.i < self.x:
+            i = self.i
+            self.i += 1
+            return i
+        else:
+            raise StopIteration
+    
+nums= myclass(5)
+myiter = iter(nums)
+while True:
+    try:
+        print(next(myiter))
+    except StopIteration:
+        break
--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,53 +1,24 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
+class myclass:
+    def __init__(self, x):
+        self.x = x
 
-    def speak(self):
-        raise NotImplementedError("Subclass must implement abstract method")
+    def __iter__(self):
+        self.i = 0
+        return self
 
-    def eat(self):
-        return f"{self.name} is eating."
-
-    def sleep(self):
-        return f"{self.name} is sleeping."
-
-class Dog(Animal):
-    def speak(self):
-        return "Woof!"
-
-    def fetch(self):
-        return f"{self.name} is fetching."
-
-class Cat(Animal):
-    def speak(self):
-        return "Meow!"
-
-    def scratch(self):
-        return f"{self.name} is scratching."
-
-class Bird(Animal):
-    def speak(self):
-        return "Chirp!"
-
-    def fly(self):
-        return f"{self.name} is flying."
-
-# Example usage
-dog = Dog("Buddy")
-cat = Cat("Whiskers")
-bird = Bird("Tweety")
-
-print(dog.speak())
-print(dog.eat())
-print(dog.sleep())
-print(dog.fetch())
-
-print(cat.speak())
-print(cat.eat())
-print(cat.sleep())
-print(cat.scratch())
-
-print(bird.speak())
-print(bird.eat())
-print(bird.sleep())
-print(bird.fly())
+    def __next__(self):
+        if self.i < self.x:
+            i = self.i
+            self.i += 1
+            return i
+        else:
+            raise StopIteration
+    
+nums= myclass(5)
+myiter = iter(nums)
+while True:
+    try:
+        print(next(myiter))
+    except StopIteration:
+        break
+    
